552e2382398e9b5a6ff15ee8064f80995615a585
Samuel De Backer <sdebacker@gmail.com>
2015-02-20 01:32:47 +0100

    licence + sidebar

--- src/Composers/SideBarViewComposer.php
+++ src/Composers/SideBarViewComposer.php
@@ -1,18 +1,25 @@
 <?php
 namespace TypiCMS\Modules\Translations\Composers;
 
-use Illuminate\View\View;
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Sidebar\SidebarGroup;
+use Maatwebsite\Sidebar\SidebarItem;
+use TypiCMS\Composers\BaseSidebarViewComposer;
 
-class SidebarViewComposer
+class SidebarViewComposer extends BaseSidebarViewComposer
 {
     public function compose(View $view)
     {
-        $view->menus['content']->put('translations', [
-            'weight' => config('typicms.translations.sidebar.weight'),
-            'request' => $view->prefix . '/translations*',
-            'route' => 'admin.translations.index',
-            'icon-class' => 'icon fa fa-fw fa-comments',
-            'title' => 'Translations',
-        ]);
+        $view->sidebar->group(trans('global.menus.content'), function (SidebarGroup $group) {
+            $group->addItem(trans('translations::global.name'), function (SidebarItem $item) {
+                $item->icon = config('typicms.translations.sidebar.icon', 'icon fa fa-fw fa-comments');
+                $item->weight = config('typicms.translations.sidebar.weight');
+                $item->route('admin.translations.index');
+                $item->append('admin.translations.create');
+                $item->authorize(
+                    $this->auth->hasAccess('translations.index')
+                );
+            });
+        });
     }
 }
