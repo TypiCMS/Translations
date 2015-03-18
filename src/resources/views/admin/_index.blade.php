3efc22c7920b38e898e4f78f46a67926f5b139cb
Samuel De Backer <sdebacker@gmail.com>
2015-03-18 23:12:32 +0100

    _index.blade

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -1,8 +1,8 @@
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
     <h1>
-        <a href="{{ url }}/create" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
-        <span translate translate-n="models.length" translate-plural="{{ models.length }} translations">{{ models.length }} translation</span>
+        <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
+        <span translate translate-n="models.length" translate-plural="@{{ models.length }} translations">@{{ models.length }} translation</span>
     </h1>
 
     <div class="btn-toolbar" role="toolbar" ng-include="'/views/partials/btnLocales.html'"></div>
@@ -20,10 +20,10 @@
                 <tr>
                     <td colspan="2"></td>
                     <td>
-                        <input st-search="key" class="form-control input-sm" placeholder="{{ 'Search' | translate }}…" type="text">
+                        <input st-search="key" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
                     </td>
                     <td>
-                        <input st-search="translation" class="form-control input-sm" placeholder="{{ 'Search' | translate }}…" type="text">
+                        <input st-search="translation" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
                     </td>
                 </tr>
             </thead>
@@ -32,9 +32,9 @@
                 <tr ng-repeat="model in displayedModels">
                     <td><typi-btn-delete ng-click="delete(model, model.key)"></typi-btn-delete></td>
                     <td typi-btn-edit></td>
-                    <td>{{ model.key }}</td>
+                    <td>@{{ model.key }}</td>
                     <td contentEditable highlighter="model.translation" ng-model="model.translation" ng-blur="update(model)">
-                        {{ model.translation }}
+                        @{{ model.translation }}
                     </td>
                 </tr>
             </tbody>
