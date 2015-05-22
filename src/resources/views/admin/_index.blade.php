638c0642ba7d38aa68fce6f09588779607cdd660
Samuel De Backer <sdebacker@gmail.com>
2015-05-22 22:14:29 +0200

    angular gettext removed

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -1,8 +1,8 @@
 <div ng-app="typicms" ng-cloak ng-controller="ListController">
 
     <h1>
-        <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only" translate>New</span></a>
-        <span translate translate-n="models.length" translate-plural="@{{ models.length }} translations">@{{ models.length }} translation</span>
+        <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only">New</span></a>
+        <span>@{{ models.length }} @choice('translations::global.translations', 2)</span>
     </h1>
 
     <div class="btn-toolbar" role="toolbar" ng-include="'/views/partials/btnLocales.html'"></div>
@@ -14,16 +14,16 @@
                 <tr>
                     <th class="delete"></th>
                     <th class="edit"></th>
-                    <th st-sort="key" st-sort-default="true" class="key st-sort" translate>Key</th>
-                    <th st-sort="translation" class="translation st-sort" translate>Translation</th>
+                    <th st-sort="key" st-sort-default="true" class="key st-sort">Key</th>
+                    <th st-sort="translation" class="translation st-sort">Translation</th>
                 </tr>
                 <tr>
                     <td colspan="2"></td>
                     <td>
-                        <input st-search="key" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
+                        <input st-search="key" class="form-control input-sm" placeholder="@lang('global.Search')…" type="text">
                     </td>
                     <td>
-                        <input st-search="translation" class="form-control input-sm" placeholder="@{{ 'Search' | translate }}…" type="text">
+                        <input st-search="translation" class="form-control input-sm" placeholder="@lang('global.Search')…" type="text">
                     </td>
                 </tr>
             </thead>
