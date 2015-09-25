2a8ffa9bc30d47c8187037bcfcb1db93303ddb64
Samuel De Backer <sdebacker@gmail.com>
2015-09-25 19:46:20 +0200

    Api FormRequest

--- src/Http/Controllers/ApiController.php
+++ src/Http/Controllers/ApiController.php
@@ -2,6 +2,7 @@
 namespace TypiCMS\Modules\Translations\Http\Controllers;
 
 use TypiCMS\Modules\Core\Http\Controllers\BaseApiController;
+use TypiCMS\Modules\Translations\Http\Requests\FormRequest;
 use TypiCMS\Modules\Translations\Repositories\TranslationInterface as Repository;
 
 class ApiController extends BaseApiController
@@ -20,5 +21,36 @@ class ApiController extends BaseApiController
     {
         $models = $this->repository->all([], true);
         return response()->json($models, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  FormRequest $request
+     * @return Model|false
+     */
+    public function store(FormRequest $request)
+    {
+        $model = $this->repository->create(Input::all());
+        $error = $model ? false : true ;
+        return response()->json([
+            'error' => $error,
+            'model' => $model,
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  $model
+     * @param  FormRequest $request
+     * @return boolean
+     */
+    public function update($model, FormRequest $request)
+    {
+        $error = $this->repository->update($request->all()) ? false : true ;
+        return response()->json([
+            'error' => $error,
+        ], 200);
     }
 }
