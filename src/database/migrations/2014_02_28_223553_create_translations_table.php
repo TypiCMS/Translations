757162ba57527fe287a87de35efcd516b13f158b
Samuel De Backer <sdebacker@gmail.com>
2016-01-18 21:47:49 +0100

    migration style

--- src/database/migrations/2014_02_28_223553_create_translations_table.php
+++ src/database/migrations/2014_02_28_223553_create_translations_table.php
@@ -22,19 +22,13 @@ class CreateTranslationsTable extends Migration
 
         Schema::create('translation_translations', function (Blueprint $table) {
             $table->engine = 'InnoDB';
-
             $table->increments('id');
             $table->integer('translation_id')->unsigned();
-
             $table->string('locale')->index();
-
             $table->text('translation')->nullable();
-
             $table->timestamps();
-
             $table->unique(['translation_id', 'locale']);
             $table->foreign('translation_id')->references('id')->on('translations')->onDelete('cascade');
-
         });
     }
 
