60adb1230777826f68d0712334c2c736f9708748
Rafael Perelló Rowley <rperellorow@gmail.com>
2016-03-16 12:10:15 +0100

    Added translation to spanish

---
+++ src/resources/lang/es/global.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name'         => 'Traducciones',
+    'translations' => 'traduccion|traducciones',
+    'New'          => 'Nueva traducción',
+    'Edit'         => 'Editar traducción',
+    'Back'         => 'Volver a las traducciones',
+];
