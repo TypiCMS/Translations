49b07b0adbbabde5542d91128ec8eac0665ba2d8
Samuel De Backer <sdebacker@gmail.com>
2016-04-15 18:05:39 +0200

    prepare for spatie translatable

--- src/Models/Translation.php
+++ src/Models/Translation.php
@@ -2,23 +2,20 @@
 
 namespace TypiCMS\Modules\Translations\Models;
 
-use Dimsav\Translatable\Translatable;
 use Laracasts\Presenter\PresentableTrait;
+use Spatie\Translatable\HasTranslations;
 use TypiCMS\Modules\Core\Models\Base;
 use TypiCMS\Modules\History\Traits\Historable;
 
 class Translation extends Base
 {
+    use HasTranslations;
     use Historable;
     use PresentableTrait;
-    use Translatable;
 
     protected $presenter = 'TypiCMS\Modules\Translations\Presenters\ModulePresenter';
 
-    protected $fillable = [
-        'group',
-        'key',
-    ];
+    protected $guarded = ['id'];
 
     public $translatable = [
         'translation',
