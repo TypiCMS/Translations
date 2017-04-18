b2aad6bc83df48b8c44e6fcd3551e0b9c5111cfb
Samuel De Backer <sdebacker@gmail.com>
2017-04-18 23:50:37 +0200

    Simpler permissions

--- src/config/permissions.php
+++ src/config/permissions.php
@@ -2,11 +2,9 @@
 
 return [
     'translations' => [
-        'index-translations' => 'Show list',
+        'index-translations' => 'View list',
         'create-translation' => 'Create',
-        'store-translation' => 'Store',
         'edit-translation' => 'Edit',
-        'update-translation' => 'Update',
         'destroy-translation' => 'Destroy',
     ],
 ];
