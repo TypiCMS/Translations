a7789fa91b668be9aad057c8692003e1ef936855
Samuel De Backer <sdebacker@gmail.com>
2017-04-18 16:35:25 +0200

    sidebar group translation

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -11,7 +11,7 @@ class SidebarViewComposer
 {
     public function compose(View $view)
     {
-        $view->sidebar->group(__('global.menus.content'), function (SidebarGroup $group) {
+        $view->sidebar->group(__('Content'), function (SidebarGroup $group) {
             $group->addItem(__('translations::global.name'), function (SidebarItem $item) {
                 $item->id = 'translations';
                 $item->icon = config('typicms.translations.sidebar.icon', 'icon fa fa-fw fa-comments');
