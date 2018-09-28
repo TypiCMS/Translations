3ac8bf42d132f67767f3a07e02990ef0d57ae868
Samuel De Backer <sdebacker@gmail.com>
2018-09-28 17:32:37 +0200

    header markup

--- src/resources/views/admin/create.blade.php
+++ src/resources/views/admin/create.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
 
-    @include('core::admin._button-back', ['module' => 'translations'])
-    <h1>
-        @lang('New translation')
-    </h1>
+    <div class="header">
+        @include('core::admin._button-back', ['module' => 'translations'])
+        <h1 class="header-title">@lang('New translation')</h1>
+    </div>
 
     {!! BootForm::open()->action(route('admin::index-translations'))->multipart()->role('form') !!}
         @include('translations::admin._form')
