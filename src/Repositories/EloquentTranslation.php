58e2d12b68cb5d09557e4d5cd84a0b0366e040ea
Samuel De Backer <sdebacker@gmail.com>
2019-04-05 14:42:40 +0200

    get_called_class replaced

--- src/Repositories/EloquentTranslation.php
+++ src/Repositories/EloquentTranslation.php
@@ -22,7 +22,7 @@ class EloquentTranslation extends EloquentRepository
         $args = func_get_args();
         $args[] = config('app.locale');
 
-        return $this->executeCallback(get_called_class(), __FUNCTION__, $args, function () use ($locale, $group) {
+        return $this->executeCallback(static::class, __FUNCTION__, $args, function () use ($locale, $group) {
             $array = DB::table('translations')
                     ->select(DB::raw("JSON_UNQUOTE(JSON_EXTRACT(`translation`, '$.".$locale."')) AS translation"), 'key')
                     ->where('group', $group)
