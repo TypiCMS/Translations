7474a198f00225f870f454d50e369815009eaaba
Samuel De Backer <sdebacker@gmail.com>
2020-06-11 22:38:18 +0200

    Get translation from database even without 'db.' prefix.

--- src/Loaders/MixedLoader.php
+++ src/Loaders/MixedLoader.php
@@ -2,7 +2,6 @@
 
 namespace TypiCMS\Modules\Translations\Loaders;
 
-use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Translation\FileLoader;
 use TypiCMS\Modules\Translations\Models\Translation;
@@ -10,56 +9,38 @@ use TypiCMS\Modules\Translations\Models\Translation;
 class MixedLoader extends FileLoader
 {
     /**
-     * Create a new file loader instance.
+     * Load the messages for the given locale.
      *
-     * @param string $path
+     * @param string      $locale
+     * @param string      $group
+     * @param string|null $namespace
      */
-    public function __construct(Filesystem $files, $path)
+    public function load($locale, $group, $namespace = null): array
     {
-        $this->path = $path;
-        $this->files = $files;
+        if (is_null($namespace) || $namespace === '*' && $group === 'db') {
+            return $this->loadFromDatabase($locale, $group, $namespace);
+        }
+
+        if ($group === '*' && $namespace === '*') {
+            $jsonTranslations = $this->loadJsonPaths($locale);
+            $databaseTranslations = $this->loadFromDatabase($locale, $group, $namespace);
+
+            return array_replace_recursive($jsonTranslations, $databaseTranslations);
+        }
+
+        if (is_null($namespace) || $namespace === '*') {
+            return $this->loadPath($this->path, $locale, $group);
+        }
+
+        return $this->loadNamespaced($locale, $group, $namespace);
     }
 
     /**
-     * Load the messages strictly for the given locale.
-     *
-     * @param string $locale
-     * @param string $group
-     * @param string $namespace
-     *
-     * @return array
+     * Load the messages from the database.
      */
-    public function load($locale, $group, $namespace = null)
-    {
-        // Load from files
-        if ($group == '*' && $namespace == '*') {
-            return $this->loadJsonPaths($locale);
-        }
-        if (is_null($namespace) || $namespace == '*') {
-            $translationsFromFiles = $this->loadPath($this->path, $locale, $group);
-        } else {
-            $translationsFromFiles = $this->loadNamespaced($locale, $group, $namespace);
-        }
-
-        // If group is 'db', get data from DB.
-        $translationsFromDB = ($group == 'db') ? $this->loadFromDB($locale, $group, $namespace) : [];
-
-        return array_merge($translationsFromFiles, $translationsFromDB);
-    }
-
-    /**
-     * Load the messages from DB strictly for the given locale.
-     *
-     * @param string $group
-     * @param string $namespace
-     * @param string $locale
-     *
-     * @return array
-     */
-    public function loadFromDB($locale, $group, $namespace = null)
+    public function loadFromDatabase(string $locale, string $group, string $namespace = null): array
     {
         return Translation::select(DB::raw("JSON_UNQUOTE(JSON_EXTRACT(`translation`, '$.".$locale."')) AS translated"), 'key')
-                ->where('group', $group)
                 ->pluck('translated', 'key')
                 ->all();
     }
