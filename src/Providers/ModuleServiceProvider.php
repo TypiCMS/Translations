1dddd7a2709acdecdb8f9122c7362e020ca074b5
Samuel De Backer <sdebacker@gmail.com>
2020-09-18 17:28:36 +0200

    ModuleServiceProvider in place of ModuleProvider

--- src/Providers/ModuleServiceProvider.php
+++ src/Providers/ModuleServiceProvider.php
@@ -5,7 +5,7 @@ namespace TypiCMS\Modules\Translations\Providers;
 use Illuminate\Support\ServiceProvider;
 use TypiCMS\Modules\Translations\Composers\SidebarViewComposer;
 
-class ModuleProvider extends ServiceProvider
+class ModuleServiceProvider extends ServiceProvider
 {
     public function boot()
     {
