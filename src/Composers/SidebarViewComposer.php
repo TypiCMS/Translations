ede910e33f71945fcdb7c649b8811182dc77b657
Samuel De Backer <sdebacker@gmail.com>
2020-09-22 16:11:39 +0200

    sidebar icon

--- src/Composers/SidebarViewComposer.php
+++ src/Composers/SidebarViewComposer.php
@@ -19,7 +19,7 @@ class SidebarViewComposer
             $group->weight = 30;
             $group->addItem(__('Translations'), function (SidebarItem $item) {
                 $item->id = 'translations';
-                $item->icon = config('typicms.translations.sidebar.icon', 'icon fa fa-fw fa-comments');
+                $item->icon = config('typicms.translations.sidebar.icon');
                 $item->weight = config('typicms.translations.sidebar.weight');
                 $item->route('admin::index-translations');
                 $item->append('admin::create-translation');
