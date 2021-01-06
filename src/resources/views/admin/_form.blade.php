3134ebd1beee05337c31cd7e5530acc5191fcf60
Samuel De Backer <sdebacker@gmail.com>
2021-01-06 13:18:12 +0100

    BS5

--- src/resources/views/admin/_form.blade.php
+++ src/resources/views/admin/_form.blade.php
@@ -9,14 +9,12 @@
 {!! BootForm::text(__('Key'), 'key')->required() !!}
 @endif
 
-<label class="control-label">{{ __('Translations') }}</label>
+<label class="form-label">{{ __('Translations') }}</label>
 
 @foreach ($locales as $lang)
     <div class="mb-3">
         <div class="input-group">
-            <div class="input-group-prepend">
-                <span class="input-group-text">{{ strtoupper($lang) }}</span>
-            </div>
+            <span class="input-group-text">{{ strtoupper($lang) }}</span>
             {!! Form::text('translation['.$lang.']')->addClass('form-control')->addClass($errors->has('translation.'.$lang) ? 'is-invalid' : '') !!}
             {!! $errors->first('translation.'.$lang, '<div class="invalid-feedback">:message</div>') !!}
         </div>
